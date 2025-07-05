c5b65fa49dd901c2f8fd15d4f179a9c906047b5e
Ricky Jiang <ricky.jc2022@gmail.com>
2025-07-05 14:12:31 +0800

    chore: initial project skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Access Control Module
+
+> Role-based access control implementation with UML documentation
---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+export default {
+  "*.{ts,mts}": ["eslint --fix", "prettier --write"],
+  "*.{js,json,md}": ["prettier --write"],
+};
