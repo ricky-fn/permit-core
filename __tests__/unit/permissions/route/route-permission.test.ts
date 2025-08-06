e9a2b8e10c49ba35001a3d3d9326feee05015d6c
Ricky Jiang <ricky.jc2022@gmail.com>
2025-08-06 17:16:35 +0800

    test: update AccessControl instantiation to include group parameter

--- __tests__/unit/permissions/route/route-permission.test.ts
+++ __tests__/unit/permissions/route/route-permission.test.ts
@@ -1,100 +1,100 @@
 import { describe, it, expect, vi } from "vitest";
 import {
-  IRoutePermissionRule,
-  RouteAccessAction,
-  RouteAccessPermission,
+	RouteAccessAction,
+	RouteAccessPermission,
+	type IRoutePermissionRule,
 } from "../../../../src/permissions";
 import {
-  AccessControl,
-  Group,
-  PermissionMessage,
-  Role,
+	AccessControl,
+	Group,
+	PermissionMessage,
+	Role,
 } from "../../../../src/core";
 
 describe("RouteAccessAction Class", () => {
-  it("should set the type to navigation", () => {
-    const action = new RouteAccessAction("ADMIN", { route: "random" });
+	it("should set the type to navigation", () => {
+		const action = new RouteAccessAction("ADMIN", { route: "random" });
 
-    expect(action.getType()).toEqual("navigation");
-  });
+		expect(action.getType()).toEqual("navigation");
+	});
 });
 
 describe("RouteAccessPermission Class", () => {
-  it("should have a type as navigation", () => {
-    const adminRole = new Role("ADMIN");
-    const permission = new RouteAccessPermission(adminRole, []);
+	it("should have a type as navigation", () => {
+		const adminRole = new Role("ADMIN");
+		const permission = new RouteAccessPermission(adminRole, []);
 
-    expect(permission.getType()).toBe("navigation");
-  });
-  it("should return the default route via isDefault flag", () => {
-    const adminRole = new Role("ADMIN");
-    const ruleWithDefaultFlag: IRoutePermissionRule = {
-      route: "random",
-      isDefault: true,
-    };
-    const rule: IRoutePermissionRule = { route: "random" };
-    const permission = new RouteAccessPermission(adminRole, [
-      rule,
-      ruleWithDefaultFlag,
-    ]);
+		expect(permission.getType()).toBe("navigation");
+	});
+	it("should return the default route via isDefault flag", () => {
+		const adminRole = new Role("ADMIN");
+		const ruleWithDefaultFlag: IRoutePermissionRule = {
+			route: "random",
+			isDefault: true,
+		};
+		const rule: IRoutePermissionRule = { route: "random" };
+		const permission = new RouteAccessPermission(adminRole, [
+			rule,
+			ruleWithDefaultFlag,
+		]);
 
-    expect(permission.getDefaultRoute()).toEqual(ruleWithDefaultFlag);
-  });
-  it("should return error if the action type is not navigation", () => {
-    const adminRole = new Role("ADMIN");
-    const permission = new RouteAccessPermission(adminRole, []);
-    const action = new RouteAccessAction("ADMIN", { route: "random" });
+		expect(permission.getDefaultRoute()).toEqual(ruleWithDefaultFlag);
+	});
+	it("should return error if the action type is not navigation", () => {
+		const adminRole = new Role("ADMIN");
+		const permission = new RouteAccessPermission(adminRole, []);
+		const action = new RouteAccessAction("ADMIN", { route: "random" });
 
-    expect(permission.validate(action)).toBeInstanceOf(PermissionMessage);
-  });
-  it("should call the functions from the middlewares", () => {
-    const adminRole = new Role("ADMIN");
-    const middlewareMock = vi.fn();
-    const permission = new RouteAccessPermission(
-      adminRole,
-      [],
-      [middlewareMock],
-    );
-    const action = new RouteAccessAction("ADMIN", { route: "random" });
+		expect(permission.validate(action)).toBeInstanceOf(PermissionMessage);
+	});
+	it("should call the functions from the middlewares", () => {
+		const adminRole = new Role("ADMIN");
+		const middlewareMock = vi.fn();
+		const permission = new RouteAccessPermission(
+			adminRole,
+			[],
+			[middlewareMock],
+		);
+		const action = new RouteAccessAction("ADMIN", { route: "random" });
 
-    permission.validate(action);
+		permission.validate(action);
 
-    expect(middlewareMock).toHaveBeenCalledOnce();
-    expect(middlewareMock).toHaveBeenCalledWith(permission, action);
-  });
-  it("should return error if the exclude flag exist", () => {
-    const adminRole = new Role("ADMIN");
-    const ruleWithExcludeFlag: IRoutePermissionRule = {
-      route: "random",
-      exclude: true,
-    };
-    const rule: IRoutePermissionRule = { route: "random" };
-    const permission = new RouteAccessPermission(adminRole, [
-      rule,
-      ruleWithExcludeFlag,
-    ]);
-    const action = new RouteAccessAction("ADMIN", { route: "random" });
+		expect(middlewareMock).toHaveBeenCalledOnce();
+		expect(middlewareMock).toHaveBeenCalledWith(permission, action);
+	});
+	it("should return error if the exclude flag exist", () => {
+		const adminRole = new Role("ADMIN");
+		const ruleWithExcludeFlag: IRoutePermissionRule = {
+			route: "random",
+			exclude: true,
+		};
+		const rule: IRoutePermissionRule = { route: "random" };
+		const permission = new RouteAccessPermission(adminRole, [
+			rule,
+			ruleWithExcludeFlag,
+		]);
+		const action = new RouteAccessAction("ADMIN", { route: "random" });
 
-    expect(permission.validate(action)).toBeInstanceOf(PermissionMessage);
-  });
-  it("should return error if the exclude flag exist in the group permission", () => {
-    const adminRole = new Role("ADMIN");
-    const group = new Group("MAIN");
-    const accessControl = new AccessControl([adminRole]);
+		expect(permission.validate(action)).toBeInstanceOf(PermissionMessage);
+	});
+	it("should return error if the exclude flag exist in the group permission", () => {
+		const adminRole = new Role("ADMIN");
+		const group = new Group("MAIN");
+		const accessControl = new AccessControl([adminRole], [group]);
 
-    group.assignRole(adminRole);
+		group.assignRole(adminRole);
 
-    const action = new RouteAccessAction("ADMIN", { route: "random" });
+		const action = new RouteAccessAction("ADMIN", { route: "random" });
 
-    const mockFailure = vi.fn();
-    const mockSuccess = vi.fn();
+		const mockFailure = vi.fn();
+		const mockSuccess = vi.fn();
 
-    accessControl.checkPermissions(action, {
-      onSuccess: mockSuccess,
-      onFailure: mockFailure,
-    });
+		accessControl.checkPermissions(action, {
+			onSuccess: mockSuccess,
+			onFailure: mockFailure,
+		});
 
-    expect(mockFailure).toBeCalledWith(action, expect.any(Object));
-    expect(mockSuccess).not.toHaveBeenCalled();
-  });
+		expect(mockFailure).toBeCalledWith(action, expect.any(Object));
+		expect(mockSuccess).not.toHaveBeenCalled();
+	});
 });
